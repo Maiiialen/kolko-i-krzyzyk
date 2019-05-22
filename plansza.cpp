9f8596d518e04544ed0a17798a9c52269a6eee37
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2019-05-22 21:06:51 +0200

    oknstruktor, destruktor, wstępne wyświetlanie planszy

--- plansza.cpp
+++ plansza.cpp
@@ -3,10 +3,41 @@
 using namespace std;
 
 class Plansza{
-    char **macierz;
+    char **Macierz;
+    int wielkosc;
 
     public:
-    Plansza();
+    Plansza(int wielkosc);
     ~Plansza();
     int Czy_wygrana();
+    void Wyswietlanie();
 };
+
+Plansza::Plansza(int wiel){
+    wielkosc = wiel;
+    Macierz = new char* [wielkosc];
+    for(int i = 0; i < wielkosc; ++i){
+        Macierz[i] = new char [wielkosc];
+    }
+    for(int i = 0; i < wielkosc; ++i){
+        for(int j = 0; j < wielkosc; ++j){
+            Macierz[i][j] = ' ';
+        }
+    }
+}
+
+Plansza::~Plansza(){
+    for(int i = 0; i < wielkosc; ++i){
+        delete Macierz[i];
+    }
+    delete Macierz;
+}
+
+void Plansza::Wyswietlanie(){
+    for(int i = 0; i < wielkosc; ++i){
+        for(int j = 0; j < wielkosc; ++j){
+            cout << Macierz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
