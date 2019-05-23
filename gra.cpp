991c7e41a88b98042a94cb556956cdb8ffc687fa
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2019-05-23 23:07:35 +0200

    minmax coś robi

--- gra.cpp
+++ gra.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <climits>
 #include "gracz.cpp"
 #include "plansza.cpp"
 
@@ -6,7 +8,8 @@ using namespace std;
 
 class Gra{
     int Wielkosc_planszy;
-    char Obecny_gracz;
+    Gracz *Obecny_gracz;
+    Gracz *Przeciwny_gracz;
     int Warunek_wygranej;
     int Tryb_gry;
     Gracz Pierwszy;
@@ -15,27 +18,101 @@ class Gra{
 
     public:
     Gra(char znak, int wielkosc, int warunek, int tryb);
-    //~Gra();
     void Rozgrywka();
     void Minmax();
+    int MaxSzukany();
+    int MinSzukany();
 };
 
 Gra::Gra(char znak, int wielkosc, int warunek, int tryb){
     Wielkosc_planszy = wielkosc;
-    Obecny_gracz = znak;
     Warunek_wygranej = warunek;
     Tryb_gry = tryb;
     plansza = new Plansza(Wielkosc_planszy);
     Pierwszy.Ustaw_znak(znak);
-    if(znak == 'O')
+    Obecny_gracz = &Pierwszy;
+    if(znak == 'O'){
         Drugi.Ustaw_znak('X');
-    else
+        Przeciwny_gracz = &Drugi;
+    } else {
         Drugi.Ustaw_znak('O');
+        Przeciwny_gracz = &Drugi;
+    }
 }
 
 
 void Gra::Rozgrywka(){
+    Gracz *tymczasowy = Obecny_gracz;
     plansza->Macierz[1][1] = Pierwszy.Podaj_znak();
     plansza->Macierz[2][2] = Drugi.Podaj_znak();
     plansza->Wyswietlanie();
+    cout << endl;
+    Minmax();
+    Obecny_gracz = Przeciwny_gracz;
+    Przeciwny_gracz = tymczasowy;
+    Minmax();
+    plansza->Wyswietlanie();
+}
+
+void Gra::Minmax(){
+    int wynik = INT_MAX, zmienna, x = -1, y = -1;
+    for(int i = 0; i < Wielkosc_planszy; ++i){
+        for(int j = 0; j < Wielkosc_planszy; ++j){
+            if(plansza->Macierz[i][j] == ' '){
+                plansza->Macierz[i][j] = Obecny_gracz->Podaj_znak();
+                zmienna = MaxSzukany();
+                if(zmienna < wynik){
+                    wynik = zmienna;
+                    x = i;
+                    y = j;
+                }
+                plansza->Macierz[i][j] = ' ';
+            }
+        }
+    }
+    cout << Obecny_gracz->Podaj_znak() << endl;
+    cout << "aaa " << x << ", " << y << endl;
+    plansza->Macierz[x][y] = Obecny_gracz->Podaj_znak();
+}
+
+int Gra::MaxSzukany(){/*
+    if(Wygrana(Przeciwny_gracz->Podaj_znak()))
+        return 10;
+    else if(Obecny_gracz->Podaj_znak())
+        return -10;
+    else if(Remis())
+        return 0;
+*/
+    int wynik = INT_MIN;
+    for(int i = 0; i < Wielkosc_planszy; ++i){
+        for(int j = 0; j < Wielkosc_planszy; ++j){
+            if(plansza->Macierz[i][j] == ' '){
+                plansza->Macierz[i][j] = Przeciwny_gracz->Podaj_znak();
+                wynik = max(wynik, MinSzukany());
+                plansza->Macierz[i][j] = ' ';
+            }
+        }
+    }
+    return wynik;
+}
+
+int Gra::MinSzukany(){/*
+    if(Wygrana(Przeciwny_gracz->Podaj_znak()))
+        return 10;
+    else if(Obecny_gracz->Podaj_znak())
+        return -10;
+    else if(Remis())
+        return 0;
+*/
+    int wynik = INT_MIN;
+    for(int i = 0; i < Wielkosc_planszy; ++i){
+        for(int j = 0; j < Wielkosc_planszy; ++j){
+            if(plansza->Macierz[i][j] == ' '){
+                plansza->Macierz[i][j] = Obecny_gracz->Podaj_znak();
+                wynik = min(wynik, MaxSzukany());
+                plansza->Macierz[i][j] = ' ';
+            }
+        }
+    }
+    return wynik;
 }
